09ce4d13c2d43a8b763573c018b3d6efa5eb46fc
dimi7ri <vasiliev.dimi@gmail.com>
2018-08-25 23:18:35 +1000

    Update to "anything goes" SchemaType.

--- src/models/core.js
+++ src/models/core.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
-const anythingSchema = new mongoose.Schema({  any: Object});
+var anythingSchema = new mongoose.Schema({ any: {} });
+var anythingSchema = new mongoose.Schema({ any: Object });
+var anythingSchema = new mongoose.Schema({ any: mongoose.Schema.Types.Mixed });
 
 export default anythingSchema;
